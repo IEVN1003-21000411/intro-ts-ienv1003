26d111f863e798e47158641ba78a786caa53b826
Eduardo Sandoval <hassaelarmenta@gmail.com>
2025-09-05 18:44:28 -0600

    Segundo commit 05-09-25

---
+++ src/temasTS/01-tipos-bas.ts
@@ -0,0 +1,22 @@
+console.log("Hola mundo!");
+
+var num1 = 23;
+let num2 = 32;
+const num3 = 67;
+
+
+let nombre:string;
+let num:number;
+let activo:boolean;
+
+let sum = num1 + num2;
+console.log("La suma es: " + sum);
+
+
+let matricula:number | string;
+matricula = 21000411;
+matricula = '21000411';
+
+console.log(matricula);
+
+
